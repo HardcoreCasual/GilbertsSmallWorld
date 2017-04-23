2ca812ee27ea792f26929c351c3bad14413b7b3a
MarcusLonabaughCST150 <mlona0003@mail.ct.edu>
2017-04-22 21:22:38 -0400

    Gilbert 3
    Space level prototype added

---
+++ Assets/Scripts/Character/spaceFish.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spaceFish : MonoBehaviour {
+
+    Animator fishAnim;
+    SpriteRenderer fishRenderer;
+
+	void Start ()
+    {
+        fishAnim = GetComponent<Animator>();
+        fishRenderer = GetComponent<SpriteRenderer>();
+	}
+	
+	void Update ()
+    {
+        if ((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) && (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)))
+        {
+            fishAnim.SetFloat("speed", 0);
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+            {
+                transform.RotateAround(Vector3.zero, Vector3.forward, Time.deltaTime * 200);
+                fishAnim.SetFloat("speed", 1);
+
+                fishRenderer.flipX = false;
+            }
+            else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+            {
+                transform.RotateAround(Vector3.zero, Vector3.back, Time.deltaTime * 200);
+                fishAnim.SetFloat("speed", 1);
+
+                fishRenderer.flipX = true;
+            }
+            else
+            {
+                fishAnim.SetFloat("speed", 0);
+            }
+        }
+	}
+}
